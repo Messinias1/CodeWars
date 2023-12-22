910f5a3b28570f2e09cab6ce9ccf6e9cdbbe9b59
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2023-12-21 20:24:15 -0500

    began arr chunks

---
+++ level8/chunkArr.js
@@ -0,0 +1,10 @@
+function eachCons(array, n) {
+  let arr = [];
+  for (let i = 0; i < array.length; i += n) {
+    const chunk = array.slice(i, i + n);
+    arr.push(chunk);
+  }
+  console.log(arr);
+}
+
+// enumerable magic #20- Cascading Subsets

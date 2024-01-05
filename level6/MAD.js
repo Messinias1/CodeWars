a71410f2c0482c2e51bd8775a381c44e18ef7751
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2024-01-04 21:12:38 -0500

    mad

---
+++ level6/MAD.js
@@ -0,0 +1,22 @@
+function gettingMad(array) {
+  let sumsArr = [];
+  console.log(array);
+
+  for (let i = 0; i < array.length; i++) {
+    let num = array[i];
+
+    for (let x = 0; x < array.length; x++) {
+      console.log("i", i, "x", x);
+      console.log("num", num, " sumsArr[x]", array[x]);
+      if (i != x) {
+        sumsArr.push(Math.abs(num - array[x]));
+      }
+    }
+  }
+
+  console.log(sumsArr);
+  console.log(Math.min(...sumsArr));
+  return Math.min(...sumsArr);
+}
+
+gettingMad([-10, 0, -3, 1]);

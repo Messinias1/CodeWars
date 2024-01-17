5c157cea69bf8ea6f22a3b0b91036ef72ef92087
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2024-01-17 18:19:16 -0500

    another alg

---
+++ level7/joyfulNums.js
@@ -0,0 +1,12 @@
+function numberJoy(n) {
+  let numArr = Array.from(n.toString(), Number);
+  let sum = 0;
+
+  numArr.forEach((num) => {
+    sum += num;
+  });
+
+  let reverseSum = parseInt(sum.toString().split("").reverse().join(""));
+
+  return n % sum === 0 && sum * reverseSum === n ? true : false;
+}

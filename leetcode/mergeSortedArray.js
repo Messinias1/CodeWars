de0ab3c4abb355e3fa809b0571f6696a2aecc86f
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2024-03-09 18:21:14 -0500

    test log

--- leetcode/mergeSortedArray.js
+++ leetcode/mergeSortedArray.js
@@ -9,6 +9,7 @@ var merge = function (nums1, m, nums2, n) {
   let index1 = m - 1;
   let index2 = n - 1;
   let mergeIndex = m + n - 1;
+  console.log(mergeIndex);
 
   while (index1 >= 0 && index2 >= 0) {
     if (nums1[index1] > nums2[index2]) {

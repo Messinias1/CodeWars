388e0b8faeefb69e6313136311574f0641f87517
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2024-03-21 20:59:26 -0400

    solution

--- leetcode/removeDuplicateFromArray.js
+++ leetcode/removeDuplicateFromArray.js
@@ -39,12 +39,12 @@ Explanation: Your function should return k = 5, with the first five elements of 
 It does not matter what you leave beyond the returned k (hence they are underscores).
  */
 var removeDuplicates = function (nums) {
-  for (let i = 0; i < nums.length - 1; i += 2) {
+  for (let i = 0; i < nums.length - 1; i++) {
     console.log("i", nums[i]);
     console.log("i + 1", nums[i + 1]);
     if (nums[i] === nums[i + 1]) {
       nums.splice(i, 1);
-      //   nums.splice(i + 1, 1);
+      i--;
     }
   }
   console.log(nums);

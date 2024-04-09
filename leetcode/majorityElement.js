e291ba0bdc7c2a37cabed2ab2d97e19c21bc3f42
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2024-04-08 21:39:55 -0400

    logging nums

--- leetcode/majorityElement.js
+++ leetcode/majorityElement.js
@@ -3,21 +3,7 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-  const countMap = {};
-  let maxCount = 0;
-  let mostFrequentElement = null;
-
-  nums.forEach((element) => {
-    countMap[element] = (countMap[element] || 0) + 1;
-
-    if (countMap[element] > maxCount) {
-      maxCount = countMap[element];
-      mostFrequentElement = element;
-    }
-  });
-
-  console.log(mostFrequentElement);
-  console.log(maxCount);
+  console.log(nums);
 };
 
 majorityElement([1, 2, 2, 3, 4, 5]);

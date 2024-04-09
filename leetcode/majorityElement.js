cc8e2a4c3d3712e9522e97947b5fb06649b2b192
Messinias1 <41557968+Messinias1@users.noreply.github.com>
2024-04-09 19:57:23 -0400

    added loops

--- leetcode/majorityElement.js
+++ leetcode/majorityElement.js
@@ -4,6 +4,9 @@
  */
 var majorityElement = function (nums) {
   console.log(nums);
+  for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i]);
+  }
 };
 
 majorityElement([1, 2, 2, 3, 4, 5]);
